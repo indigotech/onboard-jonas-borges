f265903b8530352057b183e17f3389b8774f2f0b
JonasBorgesLM <jonasleo92@yahoo.com.br>
2024-08-02 12:00:29 -0300

    refactor: :recycle: adicionando apollo e typescript
    O servidor foi refeito utilizando o apollo e typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { server } from "./schema/helloSchema";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const startServer = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: Number(PORT) },
+  });
+  console.log(`Server running at ${url}`);
+};
+
+startServer().catch((error) => {
+  console.error("Error starting server:", error);
+});
