264116114d67a56eda47fb953e1e880d26a4dfaa
JonasBorgesLM <jonasleo92@yahoo.com.br>
2024-08-07 16:37:38 -0300

    refactor: :recycle: retirando o await do retorno das funcoes

--- src/utils/password-utils.ts
+++ src/utils/password-utils.ts
@@ -4,9 +4,9 @@ const SALT_ROUNDS = 10;
 
 export const hashPassword = async (password: string) => {
   const salt = await bcrypt.genSalt(SALT_ROUNDS);
-  return await bcrypt.hash(password, salt);
+  return bcrypt.hash(password, salt);
 };
 
 export const comparePassword = async (password: string, hash: string) => {
-  return await bcrypt.compare(password, hash);
+  return bcrypt.compare(password, hash);
 };
