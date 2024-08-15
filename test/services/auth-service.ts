9c2d4f61dbe781c784615ae97d814706ee76f8b6
JonasBorgesLM <jonasleo92@yahoo.com.br>
2024-08-15 11:03:06 -0300

    refactor: :recycle: melhorias levantas pelo CR

--- test/services/auth-service.ts
+++ test/services/auth-service.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { generateToken } from '../../src/utils/jwt-utils.js';
-import { hashPassword } from '../../src/utils/password-utils.js';
+import { hashPassword } from '../src/utils/password-utils.js';
+import { generateToken } from '../src/utils/jwt-utils.js';
+import axios, { AxiosResponse } from 'axios';
 
 const prisma = new PrismaClient();
 
@@ -14,4 +15,89 @@ export const createAuthenticatedSession = async (name: string, email: string, bi
   const token = generateToken(user.id);
 
   return { user, token };
+};
+
+export const createUsersQuery = (limit?: number) => {
+  return `
+    query {
+      users ${limit !== undefined ? `(limit: ${limit})` : ''} {
+        id
+        name
+        email
+        birthDate
+      }
+    }
+  `;
+};
+
+export const createUserQuery = (id: string) => {
+  return `
+    query {
+      user (id: "${id}") {
+        id
+        name
+        email
+        birthDate
+      }
+    }
+  `;
+};
+
+export const createLoginMutation = (email: string, password: string) => {
+  return `
+      mutation {
+        login(input: {
+          email: "${email}",
+          password: "${password}"
+        }) {
+          user {
+            id
+            name
+            email
+            birthDate
+          }
+          token
+        }
+      }
+  `;
+};
+
+export const createUserMutation = (name: string, email: string, password: string, birthDate: string) => {
+  return `
+      mutation {
+        createUser(input: {
+          name: "${name}",
+          email: "${email}",
+          password: "${password}",
+          birthDate: "${birthDate}"
+        }) {
+          id
+          name
+          email
+          birthDate
+          createdAt
+          updatedAt
+        }
+      }
+  `;
+};
+
+export const executeGraphQLQuery = async (
+  url: string,
+  query: string,
+  token: string,
+  variables?: object,
+): Promise<AxiosResponse> => {
+  return axios.post(
+    url,
+    {
+      query,
+      variables,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
 };
